a39ba328276a43aee7f730615b19a13ac721096d
Michael Schaffer <ichaelm@case.edu>
2014-06-25 08:05:23 -0400

    Document ui_structure

--- src/vfplib/ui_structure.py
+++ src/vfplib/ui_structure.py
@@ -1,8 +1,12 @@
-'''
+"""
+Defines the classes and constants that define the structural components of the
+device's UI. Instances of these classes are built by the parser and returned to
+the crawler.
+
 Created on Jun 10, 2014
 
 @author: mschaffe
-'''
+"""
 
 NUMERICAL_ENTRY_MAP = {
     '1'      : (228, 341),
@@ -23,10 +27,21 @@ NUMERICAL_ENTRY_MAP = {
 }
 
 class Button(object):
-    #immutable!!!
-    # name
-    # coordinates
-    # pointer to target screen
+    """Represents a button on a particular screen.  Immutable.
+
+    The unique identity of a button is determined by its name and coordinates,
+    and is only valid among other buttons on the same screen. Whenever a button
+    object is hashed, that hash is guaranteed not to collide with other buttons
+    on the same screen, but there is no guarantee about buttons on different
+    screens.
+
+    Attributes:
+        name (str): The button's name on the screen, either as a label or as
+        text inside the button.
+        coord (tuple of int): The button's coordinates on the screen, ideally
+        the center.
+
+    """
     def __init__(self, name, coord):
         self.name = name
         self.coord = coord
@@ -47,6 +62,20 @@ class Button(object):
         return 'Button: ' + str(self.name)
 
 class FieldButton(Button):
+    """Represents a field-setting button on a particular screen.  Immutable.
+
+    Note that the unique identity of a field button is determined only by its
+    name and coordinates, just like a normal button. The setting can change
+    without affecting the hash value or equality of the field button.
+
+    Attributes:
+        name (str): The button's name on the screen, as a label. Inherited
+        from Button.
+        coord (tuple of int): The button's coordinates on the screen, ideally
+        the center. Inherited from Button.
+        setting (str): The button's current setting, as text inside the button.
+
+    """
     def __init__(self, name, coord, setting):
         super(FieldButton, self).__init__(name, coord)
         self.setting = setting
@@ -55,12 +84,23 @@ class FieldButton(Button):
         return 'FieldButton: ' + str(self.name)
 
 class Screen(object):
-    """ Represents an immutable and unique UI screen.
+    """Represents a screen in the UI, either a full screen or a popup screen.
+    Immutable.
 
-    Stores the screen's name as a string, and all of its buttons as Button objects.
-    Uniqueness is determined by the name and set of buttons
+    The unique identity of a screen is determined by its title, its set of
+    buttons, and its parent screen, if any.
 
-    NEW: stores mutable settings that are NOT hashed
+    Note that although the equality contract of Button uses both the button's
+    name and its coordinates, Screen's buttonmap attribute makes the
+    assumption that all buttons have a unique name.
+
+    Attributes:
+        title (str): The screen's title, generally at the top of the screen.
+        buttonmap (dict): The screen's mapping of button names to buttons.
+        parent (Screen): The screen's parent screen. This attribute is currently
+        used to store the full screen from which a popup screen is spawned.
+        Optional.
+
     """
     def __init__(self, title, buttons, parent=None):
         self.title = title
@@ -88,6 +128,21 @@ class Screen(object):
         return mystr
 
 class NumericalEntry(Screen):
+    """Represents a numerical entry screen in the UI. Immutable.
+
+    All numerical entry screens have the exact same buttonmap, derived from
+    NUMERICAL_ENTRY_BUTTONS.
+
+    Attributes:
+        title (str): The numerical entry screen's title. Inherited from
+        Screen.
+        buttonmap (dict): The screen's mapping of button names to buttons.
+        Same for all numerical entry screens.
+        parent (Screen): The numerical entry screen's parent screen. This
+        attribute is currently used to store the full screen from which this
+        screen is spawned as a popup. Optional.
+
+    """
 
     NUMERICAL_ENTRY_BUTTONS = []
     for name in NUMERICAL_ENTRY_MAP:
