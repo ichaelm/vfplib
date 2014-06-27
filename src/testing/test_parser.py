c234a481af072ecd5a2c32ca2461e5ce9ead211b
Michael Schaffer <ichaelm@case.edu>
2014-06-27 14:11:11 -0400

    Add empty unit test module for parser

---
+++ src/testing/test_parser.py
@@ -0,0 +1,64 @@
+'''
+Created on Jun 27, 2014
+
+@author: mschaffe
+'''
+import unittest
+
+import vfplib.parser as parser
+
+class Test(unittest.TestCase):
+
+    def testOCRImage(self):
+        pass  # unimplemented test
+
+    def testMatchCorners(self):
+        pass  # unimplemented test
+
+    def testQuadToBox(self):
+        pass  # unimplemented test
+
+    def testBoxArea(self):
+        pass  # unimplemented test
+
+    def testShrinkBox(self):
+        pass  # unimplemented test
+
+    def testBoxToLabelBox(self):
+        pass  # unimplemented test
+
+    def testBoxToLabelFindingBox(self):
+        pass  # unimplemented test
+
+    def testFitBoxInsideBounds(self):
+        pass  # unimplemented test
+
+    def testGetTitleBox(self):
+        pass  # unimplemented test
+
+    def testGetBoxCenter(self):
+        pass  # unimplemented test
+
+    def testGetPopupBox(self):
+        pass  # unimplemented test
+
+    def testGetButtonBoxes(self):
+        pass  # unimplemented test
+
+    def testButtonHasLabel(self):
+        pass  # unimplemented test
+
+    def testOCRTitle(self):
+        pass  # unimplemented test
+
+    def testOCRButton(self):
+        pass  # unimplemented test
+
+    def testOCRLabel(self):
+        pass  # unimplemented test
+
+    def ConvertBoxToButton(self):
+        pass  # unimplemented test
+
+if __name__ == "__main__":
+    unittest.main()
