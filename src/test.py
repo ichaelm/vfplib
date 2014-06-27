63499c3d0ac8f2c359de3fda26734d5171d8dbae
Michael Schaffer <ichaelm@case.edu>
2014-06-27 09:50:14 -0400

    Renamed test_parser to test_parser_blackbox

--- src/test.py
+++ src/test.py
@@ -6,5 +6,5 @@ Created on Jun 21, 2014
 
 import unittest
 suite = unittest.TestSuite()
-suite.addTest(unittest.defaultTestLoader.loadTestsFromName('testing.test_parser'))
+suite.addTest(unittest.defaultTestLoader.loadTestsFromName('testing.test_parser_blackbox'))
 unittest.TextTestRunner(verbosity=2).run(suite)
