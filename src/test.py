391801e0c67dd47fa156159045ea0ab42337dfb7
Michael Schaffer <ichaelm@case.edu>
2014-07-02 15:54:08 -0400

    Added empty test_graph.py

--- src/test.py
+++ src/test.py
@@ -7,4 +7,7 @@ Created on Jun 21, 2014
 import unittest
 suite = unittest.TestSuite()
 suite.addTest(unittest.defaultTestLoader.loadTestsFromName('testing.test_parser_blackbox'))
+suite.addTest(unittest.defaultTestLoader.loadTestsFromName('testing.test_parser'))
+suite.addTest(unittest.defaultTestLoader.loadTestsFromName('testing.test_ui_structure'))
+suite.addTest(unittest.defaultTestLoader.loadTestsFromName('testing.test_graph'))
 unittest.TextTestRunner(verbosity=2).run(suite)
---
+++ src/testing/test_graph.py
@@ -0,0 +1,14 @@
+'''
+Created on Jul 2, 2014
+
+@author: mschaffe
+'''
+import unittest
+
+from vfplib.graph import Graph, GraphEdge, GraphNode
+
+class Test(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
