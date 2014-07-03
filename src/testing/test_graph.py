148734e38985ab0a10557004a5ecd91257a6137f
Michael Schaffer <ichaelm@case.edu>
2014-07-03 15:50:22 -0400

    Add templates for testing GraphNode

--- src/testing/test_graph.py
+++ src/testing/test_graph.py
@@ -5,10 +5,61 @@ Created on Jul 2, 2014
 '''
 import unittest
 
-from vfplib.graph import Graph, GraphEdge, GraphNode
+from vfplib.graph import Graph, GraphNode
 
 class Test(unittest.TestCase):
-    pass
+
+    def assertEqualityContract(self, a, b, expected, message=None):
+        if message == None:
+            message = str(a) + ' and ' + str(b)
+        self.assertEqual(a == b, expected, 'Equality contract broken for: ' + message)
+        self.assertEqual(b == a, expected, 'Equality contract broken for: ' + message)
+        self.assertEqual(a != b, not expected, 'Equality contract broken for: ' + message)
+        self.assertEqual(b != a, not expected, 'Equality contract broken for: ' + message)
+
+    def assertHashContract(self, a, b, message=None):
+        if message == None:
+            message = str(a) + ' and ' + str(b)
+        if a == b:
+            self.assertEqual(hash(a), hash(b), 'Hash contract broken for: ' + message)
+        else:
+            self.assertNotEqual(hash(a), hash(b), 'Hash contract quasi-broken for: ' + message)
+
+    def testGraphNodeInit(self):
+        pass
+
+    def testGraphNodeEquality(self):
+        pass
+
+    def testGraphNodeHash(self):
+        pass
+
+    def testGraphNodeStr(self):
+        pass
+
+    def testGraphNodeAddEdge(self):
+        pass
+
+    def testGraphNodeRemoveEdge(self):
+        pass
+
+    def testGraphNodeSetEdgeTarget(self):
+        pass
+
+    def testGraphNodeGetEdgeTarget(self):
+        pass
+
+    def testGraphNodeRemoveEdgeTarget(self):
+        pass
+
+    def testGraphNodeGetEdges(self):
+        pass
+
+    def testGraphNodeGetExploredEdges(self):
+        pass
+
+    def testGraphNodeGetUnexploredEdges(self):
+        pass
 
 if __name__ == "__main__":
     unittest.main()
