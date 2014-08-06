b2963d86dfd67fa03d16c2d40bcedcd8eca84607
Michael Schaffer <ichaelm@case.edu>
2014-08-06 11:49:12 -0400

    Rearranged crawler tests

--- src/test.py
+++ src/test.py
@@ -8,6 +8,7 @@ import unittest
 suite = unittest.TestSuite()
 suite.addTest(unittest.defaultTestLoader.loadTestsFromName('testing.test_parser_blackbox'))
 suite.addTest(unittest.defaultTestLoader.loadTestsFromName('testing.test_parser'))
+suite.addTest(unittest.defaultTestLoader.loadTestsFromName('testing.test_crawler'))
 suite.addTest(unittest.defaultTestLoader.loadTestsFromName('testing.test_ui_structure'))
 suite.addTest(unittest.defaultTestLoader.loadTestsFromName('testing.test_graph'))
 unittest.TextTestRunner(verbosity=2).run(suite)
---
+++ src/testing/test_crawler_blackbox.py
@@ -0,0 +1,51 @@
+'''
+Created on Jun 4, 2014
+
+@author: mschaffe
+'''
+
+import unittest
+
+from vfplib.crawler import Crawler
+import cPickle as pickle
+from testing.dummy_parser import DummyParser, dummyscreenmap
+
+# 'test' or 'real' or 'load'
+IP = '134.63.79.71'
+LOAD_PATH = 'out.pickle'
+SAVE = False
+SAVE_PATH = 'out.pickle'
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        # dut = DummySession()
+        interp = Crawler(DummyParser())
+        graph = interp.crawl()
+
+        assert len(graph) == len(dummyscreenmap)
+        for screen in graph:
+            assert dummyscreenmap[screen.title] == screen
+            print(str(screen) + ' has buttons:')
+            for button in screen.buttonmap.values():
+                print('    ' + button.name)
+            if len(set(graph.explored_edges(screen))) > 0:
+                print('  and points:')
+                for edge in graph.explored_edges(screen):
+                    assert edge == screen.buttonmap[edge.name]
+                    # assert graph.follow_edge(screen, edge).name == edge.name.partition('_to_')[2]
+                    print('    through ' + str(edge) + ' to ' + str(graph.follow_edge(screen, edge)))
+        print(str(interp.parser.numscreencaps) + ' total screencaps')
+        if SAVE:
+            pickle.dump(graph, open(SAVE_PATH, 'wb'), protocol=2)
+    def tearDown(self):
+        pass
+
+
+    def testName(self):
+        pass
+
+if __name__ == "__main__":
+    # import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
