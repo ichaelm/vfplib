17d0651b3eab88d4cc5905bbaef1e984292ab153
Michael Schaffer <ichaelm@case.edu>
2014-08-06 15:22:42 -0400

    Add Click features to crawler
    Instead of using button objects as graph edges, the crawler now uses
    click objects as graph edges. This means that the graph edges are
    unique for buttons with different settings. Added functionality to merge
    buttons and screens. Did NOT add functionality to save information about
    effects on state variables in the click object. The crawler now
    understands that clicking a button with a different setting can bring it
    to a different screen, but it does not yet understand how those settings
    are set.

--- src/testing/test_crawler.py
+++ src/testing/test_crawler.py
@@ -4,9 +4,53 @@ Created on Aug 6, 2014
 @author: mschaffe
 '''
 import unittest
+from vfplib.ui_structure import Button, FieldButton, Screen
+import vfplib.crawler as crawler
 
 class Test(unittest.TestCase):
-    pass
+    def testMergeButtons(self):
+        a = Button('a', (0, 0))
+        b_1 = FieldButton('b', (0, 1), 'b_1')
+        b_2 = FieldButton('b', (0, 1), 'b_2')
+        diff1 = crawler.merge_buttons(b_1, b_2)
+        self.assertEquals(b_2.setting, 'b_1')
+        self.assertEquals(b_2.settings, set(['b_1', 'b_2']))
+        diff2 = crawler.merge_buttons(b_2, b_1)
+        self.assertEquals(diff1, set(['b_1']))
+        self.assertEquals(diff2, set(['b_2']))
+        self.assertEquals(b_1.setting, 'b_1')
+        self.assertEquals(b_1.settings, set(['b_1', 'b_2']))
+        # TODO: add more corner case coverage
+
+    def testMergeScreens(self):
+        ba = Button('ba', (0, 0))
+        bb_1 = FieldButton('bb', (0, 1), 'bb_1')
+        bb_2 = FieldButton('bb', (0, 1), 'bb_2')
+        bb_3 = FieldButton('bb', (0, 1), 'bb_3')
+        bc_1 = FieldButton('bc', (0, 2), 'bc_1')
+        bc_2 = FieldButton('bc', (0, 2), 'bc_2')
+        bd_1 = FieldButton('bd', (0, 3), 'bd_1')
+        a = Screen('a', [ba, bb_1, bc_1, bd_1])
+        b = Screen('a', [ba, bb_2, bc_2, bd_1])
+        c = Screen('a', [ba, bb_3, bc_1, bd_1])
+        diffs1 = crawler.merge_screens(c, b)
+        diffs2 = crawler.merge_screens(b, a)
+        self.assertEquals(diffs1, {'ba': set(), 'bb': set(['bb_3']), 'bc': set(['bc_1']), 'bd': set()})
+        self.assertEquals(diffs2, {'ba': set(), 'bb': set(['bb_2', 'bb_3']), 'bc': set(['bc_2']), 'bd': set()})
+        self.assertEquals(b.buttonmap['bb'].setting, 'bb_3')
+        self.assertEquals(b.buttonmap['bb'].settings, set(['bb_2', 'bb_3']))
+        self.assertEquals(b.buttonmap['bc'].setting, 'bc_1')
+        self.assertEquals(b.buttonmap['bc'].settings, set(['bc_1', 'bc_2']))
+        self.assertEquals(b.buttonmap['bd'].setting, 'bd_1')
+        self.assertEquals(b.buttonmap['bd'].settings, set(['bd_1']))
+        self.assertEquals(a.buttonmap['bb'].setting, 'bb_3')
+        self.assertEquals(a.buttonmap['bb'].settings, set(['bb_1', 'bb_2', 'bb_3']))
+        self.assertEquals(a.buttonmap['bc'].setting, 'bc_1')
+        self.assertEquals(a.buttonmap['bc'].settings, set(['bc_1', 'bc_2']))
+        self.assertEquals(a.buttonmap['bd'].setting, 'bd_1')
+        self.assertEquals(a.buttonmap['bd'].settings, set(['bd_1']))
+        # TODO: add more corner case coverage
+
 
 if __name__ == "__main__":
     unittest.main()
--- src/vfplib/crawler.py
+++ src/vfplib/crawler.py
@@ -5,6 +5,30 @@ Created on Jun 5, 2014
 '''
 
 from .graph import Graph
+from .ui_structure import Button, FieldButton, Click, NumericalEntry
+import random
+
+def merge_buttons(source, target):
+    diff = set()
+    assert source == target
+    if isinstance(source, FieldButton):
+        assert isinstance(target, FieldButton)
+        for sourcesetting in source.settings:
+            if sourcesetting not in target.settings:
+                target.settings.add(sourcesetting)
+                diff.add(sourcesetting)
+        target.set_setting(source.setting)
+    return diff
+
+def merge_screens(source, target):
+    diffs = {}
+    assert(source == target)
+    for buttonname in source.buttonmap:
+        sourcebutton = source.buttonmap[buttonname]
+        targetbutton = target.buttonmap[buttonname]
+        diff = merge_buttons(sourcebutton, targetbutton)
+        diffs[buttonname] = diff
+    return diffs
 
 class Crawler(object):
     def __init__(self, parser):
@@ -13,14 +37,25 @@ class Crawler(object):
         self.lastfullscreen = None
         self.lastscreen = None
         self.currentscreen = None
-        self.currentbutton = None
+        self.currentclick = None
 
-    def click(self, button):
+    def click(self, click):
+        button = click.button
+        setting = click.setting
+        if setting:
+            assert isinstance(button, FieldButton)
+        else:
+            assert not isinstance(button, FieldButton)
         if self.currentscreen == None:
             raise RuntimeError('Crawler: Must run analyze() before click()')
         if button not in self.currentscreen.buttonmap.values():
             raise RuntimeError('Crawler: Cannot click ' + str(button) + ': not found on current screen')
-        self.currentbutton = button
+        if setting:
+            if setting not in self.currentscreen.buttonmap[button.name].settings:
+                raise RuntimeError('Crawler: Cannot click ' + str(button) + ' with setting ' + str(setting) + ': not found in stored settings')
+            if setting != self.currentscreen.buttonmap[button.name].setting:
+                raise RuntimeError('Crawler: Cannot click ' + str(button) + ' with setting ' + str(setting) + ': not current setting')
+        self.currentclick = click
         self.lastscreen = self.currentscreen
         if self.lastscreen.parent == None:
             self.lastfullscreen = self.lastscreen
@@ -33,29 +68,52 @@ class Crawler(object):
 
     def analyze(self):
         screen = self.parser.analyze(self.lastfullscreen)
+        buttons = screen.buttonmap.values()
+        clicks = set()
+        for button in buttons:
+            if isinstance(button, FieldButton):
+                clicks.add(Click(button, button.setting))
+            else:
+                clicks.add(Click(button))
         if screen not in self.graph:
-            self.graph.add(screen, screen.buttonmap.values())
+            self.graph.add(screen, clicks)
+        else:
+            diffs = merge_screens(screen, self.graph.nodemap[screen].value)
+            for buttonname in diffs:
+                diff = diffs[buttonname]
+                for setting in diff:
+                    self.graph.add_edge(screen, Click(screen.buttonmap[buttonname], setting))
         self.currentscreen = screen
         if self.lastscreen != None:
-            assert self.currentbutton != None
-            self.graph.add_edge(self.lastscreen, self.currentbutton, self.currentscreen)
+            assert self.currentclick != None
+            self.graph.add_edge(self.lastscreen, self.currentclick, self.currentscreen)
         return screen
 
     def crawl(self):
         self.analyze()
         path = self.graph.nearest_unexplored_edge(self.currentscreen)
         while path != None:
-            for button in path:
-                clickedbuttons = set(self.graph.explored_edges(self.currentscreen))
-                self.click(button)
-                if button in clickedbuttons:
-                    expectedcurrentscreen = self.graph.follow_edge(self.lastscreen, button)
+            for click in path:
+                clickedclicks = set(self.graph.explored_edges(self.currentscreen))
+                currentclicks = self.currentscreen.get_current_clicks()
+                if click not in currentclicks:
+                    randomclick = random.sample(currentclicks, 1)[0]
+                    newclick = Click(self.currentscreen.buttonmap[click.button.name], self.currentscreen.buttonmap[click.button.name].setting)
+                    print('want to do ' + str(click) + ' instead doing ' + str(newclick) + ' (used to do ' + str(randomclick) + ')')
+                    click = newclick
+                self.click(click)
+                if click in clickedclicks:
+                    expectedcurrentscreen = self.graph.follow_edge(self.lastscreen, click)
                     self.analyze()
+                    break  # temp
                     if self.currentscreen != expectedcurrentscreen:
                         break
                 else:
                     self.analyze()
-            path = self.graph.nearest_unexplored_edge(self.currentscreen)
+            if isinstance(self.currentscreen, NumericalEntry):
+                path = [Click(self.currentscreen.buttonmap['Cancel'])]
+            else:
+                path = self.graph.nearest_unexplored_edge(self.currentscreen)
         return self.graph
 
     def __str__(self):
