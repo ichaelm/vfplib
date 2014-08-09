d995c73d40e918febc30adb00e39d6998e58bb9e
Michael Schaffer <ichaelm@case.edu>
2014-08-09 11:21:21 -0400

    Remove privacy paradigm from Session

--- src/vfplib/session.py
+++ src/vfplib/session.py
@@ -28,62 +28,52 @@ class Session(object):
         assert isinstance(address, str)
         # param address = string containing ip address of the device.
         # goal: store self.address, and use it to initialize a session and store the session number
-        self._address = address
+        self.address = address
         try:
-            response = urllib2.urlopen('http://' + self._address + '/ajax_proc', 'function=9').readline()
+            response = urllib2.urlopen('http://' + self.address + '/ajax_proc', 'function=9').readline()
             if '\x00' not in response:  # This string appears to indicate that the connection is new. If not new, creates a new one.
-                response = urllib2.urlopen('http://' + self._address + '/ajax_proc', 'function=9').readline()
+                response = urllib2.urlopen('http://' + self.address + '/ajax_proc', 'function=9').readline()
         except urllib2.URLError:
             # error communicating with server
             raise
         except urllib2.HTTPError:
             # server responded with error message, such as 404
             raise
-        self._sessionid = int(response.partition('=')[2].partition('$')[0])  # parses response of the form 'session=XXX$symbols'
+        self.sessionid = int(response.partition('=')[2].partition('$')[0])  # parses response of the form 'session=XXX$symbols'
         self.screencap_delay = DEFAULT_SCREENCAP_DELAY
         self.command_delay = DEFAULT_COMMAND_DELAY
         self.screencap_attempt_limit = DEFAULT_SCREENCAP_ATTEMPT_LIMIT
 
-    @property
-    def address(self):
-        """Stores the IP address of the device"""
-        return self._address
-
-    @property
-    def sessionid(self):
-        """Stores the ID number of the current session"""
-        return self._sessionid
-
     def reset(self):
         """Re-establishes connection using the stored IP address, or throws exceptions on failure."""
         try:
-            response = urllib2.urlopen('http://' + self._address + '/ajax_proc', 'function=9').readline()
+            response = urllib2.urlopen('http://' + self.address + '/ajax_proc', 'function=9').readline()
             if '\x00' not in response:  # This string appears to indicate that the connection is new. If not new, creates a new one.
-                response = urllib2.urlopen('http://' + self._address + '/ajax_proc', 'function=9').readline()
+                response = urllib2.urlopen('http://' + self.address + '/ajax_proc', 'function=9').readline()
         except urllib2.URLError:
             # error communicating with server
             raise
         except urllib2.HTTPError:
             # server responded with error message, such as 404
             raise
-        self._sessionid = int(response.partition('=')[2].partition('$')[0])  # parses response of the form 'session=XXX$symbols'
+        self.sessionid = int(response.partition('=')[2].partition('$')[0])  # parses response of the form 'session=XXX$symbols'
 
     def screencap(self):
         """Performs screen capture, including all dummy requests and delays to ensure the screen returned is what was shown at the time this function was called.
 
         Returns a PIL image object
         """
-        _ = urllib2.urlopen('http://' + self._address + '/images/fp.png?' + str(self._sessionid))
+        _ = urllib2.urlopen('http://' + self.address + '/images/fp.png?' + str(self.sessionid))
         time.sleep(self.screencap_delay)
-        # _ = urllib2.urlopen('http://' + self._address + '/images/fp.png?' + str(self._sessionid))
+        # _ = urllib2.urlopen('http://' + self.address + '/images/fp.png?' + str(self.sessionid))
         # time.sleep(self.screencap_delay)
-        data = urllib2.urlopen('http://' + self._address + '/images/fp.png?' + str(self._sessionid))
+        data = urllib2.urlopen('http://' + self.address + '/images/fp.png?' + str(self.sessionid))
         image_file = io.BytesIO(data.read())
         im = Image.open(image_file)
         i = 0
         while im.size[0] < 20:
             time.sleep(self.screencap_delay)
-            data = urllib2.urlopen('http://' + self._address + '/images/fp.png?' + str(self._sessionid))
+            data = urllib2.urlopen('http://' + self.address + '/images/fp.png?' + str(self.sessionid))
             image_file = io.BytesIO(data.read())
             im = Image.open(image_file)
             i = i + 1
@@ -93,33 +83,33 @@ class Session(object):
 
     def click(self, x, y):
         """Sends command to click at (x,y)"""
-        urllib2.urlopen('http://' + self._address + '/ajax_proc', 'function=2&x1=' + str(x) + '&y1=' + str(y) + '&session=' + str(self._sessionid))
+        urllib2.urlopen('http://' + self.address + '/ajax_proc', 'function=2&x1=' + str(x) + '&y1=' + str(y) + '&session=' + str(self.sessionid))
         time.sleep(self.command_delay)
-        urllib2.urlopen('http://' + self._address + '/ajax_proc', 'function=3&x1=' + str(x) + '&y1=' + str(y) + '&x2=' + str(x) + '&y2=' + str(y) + '&session=' + str(self._sessionid))
+        urllib2.urlopen('http://' + self.address + '/ajax_proc', 'function=3&x1=' + str(x) + '&y1=' + str(y) + '&x2=' + str(x) + '&y2=' + str(y) + '&session=' + str(self.sessionid))
         time.sleep(self.command_delay)
 
     def drag(self, x1, y1, x2, y2):
         """Sends command to drag screen from (x1,y1) to (x2,y2)."""
         # Does not work
-        urllib2.urlopen('http://' + self._address + '/ajax_proc', 'function=2&x1=' + str(x1) + '&y1=' + str(y1) + '&session=' + str(self._sessionid))
+        urllib2.urlopen('http://' + self.address + '/ajax_proc', 'function=2&x1=' + str(x1) + '&y1=' + str(y1) + '&session=' + str(self.sessionid))
         time.sleep(self.command_delay)
-        urllib2.urlopen('http://' + self._address + '/ajax_proc', 'function=4&x1=' + str(x1) + '&y1=' + str(y1) + '&x2=' + str(x2) + '&y2=' + str(y2) + '&session=' + str(self._sessionid))
+        urllib2.urlopen('http://' + self.address + '/ajax_proc', 'function=4&x1=' + str(x1) + '&y1=' + str(y1) + '&x2=' + str(x2) + '&y2=' + str(y2) + '&session=' + str(self.sessionid))
         # TODO: missing dir parameter in second command
         time.sleep(self.command_delay)
 
     def press(self, buttonnumber):
         """Sends command to press the physical button with the given number."""
-        urllib2.urlopen('http://' + self._address + '/ajax_proc', 'function=7&button=' + str(buttonnumber) + '&session=' + str(self._sessionid))
+        urllib2.urlopen('http://' + self.address + '/ajax_proc', 'function=7&button=' + str(buttonnumber) + '&session=' + str(self.sessionid))
         time.sleep(self.command_delay)
-        urllib2.urlopen('http://' + self._address + '/ajax_proc', 'function=8&button=' + str(buttonnumber) + '&session=' + str(self._sessionid))
+        urllib2.urlopen('http://' + self.address + '/ajax_proc', 'function=8&button=' + str(buttonnumber) + '&session=' + str(self.sessionid))
         time.sleep(self.command_delay)
 
     def __hash__(self):
-        return hash((self._address, self._sessionid))
+        return hash((self.address, self.sessionid))
 
     def __eq__(self, other):
         if isinstance(other, self.__class__):
-            return (self._address, self._sessionid) == (other._address, other._sessionid)
+            return (self.address, self.sessionid) == (other.address, other.sessionid)
         else:
             return False
 
@@ -127,5 +117,5 @@ class Session(object):
         return not self.__eq__(other)
 
     def __str__(self):
-        return 'VFP session #' + str(self._sessionid) + ' at ' + self._address
+        return 'VFP session #' + str(self.sessionid) + ' at ' + self.address
 
