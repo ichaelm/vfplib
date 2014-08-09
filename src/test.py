0371b2d43ce6e7d196956a3c2d198649df5af20d
Michael Schaffer <ichaelm@case.edu>
2014-08-09 13:49:07 -0400

    Add crawler blackbox test to main test runner

--- src/test.py
+++ src/test.py
@@ -6,9 +6,10 @@ Created on Jun 21, 2014
 
 import unittest
 suite = unittest.TestSuite()
-suite.addTest(unittest.defaultTestLoader.loadTestsFromName('testing.test_parser_blackbox'))
 suite.addTest(unittest.defaultTestLoader.loadTestsFromName('testing.test_parser'))
 suite.addTest(unittest.defaultTestLoader.loadTestsFromName('testing.test_crawler'))
 suite.addTest(unittest.defaultTestLoader.loadTestsFromName('testing.test_ui_structure'))
 suite.addTest(unittest.defaultTestLoader.loadTestsFromName('testing.test_graph'))
+suite.addTest(unittest.defaultTestLoader.loadTestsFromName('testing.test_parser_blackbox'))
+suite.addTest(unittest.defaultTestLoader.loadTestsFromName('testing.test_crawler_blackbox'))
 unittest.TextTestRunner(verbosity=2).run(suite)
