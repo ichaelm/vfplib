2ff2337efa0b9405f4934b1857f19f04bb5cac9a
Michael Schaffer <ichaelm@case.edu>
2014-08-09 12:55:58 -0400

    Add verbose switch to test_crawler_blackbox

--- src/testing/test_crawler_blackbox.py
+++ src/testing/test_crawler_blackbox.py
@@ -10,6 +10,8 @@ from vfplib.crawler import Crawler
 import cPickle as pickle
 from testing.dummy_parser import DummyParser, dummyscreenmap
 
+verbose = False
+
 class Test(unittest.TestCase):
 
     def iteration(self):
@@ -20,20 +22,25 @@ class Test(unittest.TestCase):
         assert len(graph) == len(dummyscreenmap)
         for screen in graph:
             assert dummyscreenmap[screen.title] == screen
-            print(str(screen) + ' has buttons:')
-            for button in screen.buttonmap.values():
-                print('    ' + button.name)
+            if verbose:
+                print(str(screen) + ' has buttons:')
+                for button in screen.buttonmap.values():
+                    print('    ' + button.name)
             if len(set(graph.explored_edges(screen))) > 0:
-                print('  and points:')
+                if verbose:
+                    print('  and points:')
                 for edge in graph.explored_edges(screen):
                     assert edge.button == screen.buttonmap[edge.button.name]
                     # assert graph.follow_edge(screen, edge).name == edge.name.partition('_to_')[2]
-                    print('    through ' + str(edge) + ' to ' + str(graph.follow_edge(screen, edge)))
-        print(str(interp.parser.numscreencaps) + ' total screencaps')
+                    if verbose:
+                        print('    through ' + str(edge) + ' to ' + str(graph.follow_edge(screen, edge)))
+        if verbose:
+            print(str(interp.parser.numscreencaps) + ' total screencaps')
 
     def testIterations(self):
         for i in xrange(1000):
-            print('ITERATION ' + str(i))
+            if verbose:
+                print('ITERATION ' + str(i))
             self.iteration()
 
 if __name__ == "__main__":
