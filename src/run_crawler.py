b1d645c1d548c56ef900b3980b6f56507113da99
Michael Schaffer <ichaelm@case.edu>
2014-08-09 09:59:45 -0400

    Extend run_crawler.py to run per-function
    run_crawler.py now runs it's crawl and help window steps on the setting screens of each function in a stored list. Many functions are currently commented out because the program is not yet able to handle them. The pickle saving and loading functionality was changed so that filenames now include the name of the function. A final step was added to run after each function is analyzed that uses the saved pickles to build a master csv file with all the saved help caption data.

--- src/run_crawler.py
+++ src/run_crawler.py
@@ -4,6 +4,8 @@ Created on Jun 10, 2014
 @author: mschaffe
 '''
 
+from __future__ import print_function
+
 from vfplib.crawler import Crawler
 from vfplib.parser import Parser
 from vfplib.session import Session
@@ -11,67 +13,103 @@ from vfplib.ui_structure import Click, NumericalEntry
 import cPickle as pickle
 
 IP = '134.63.78.160'
-LOAD_PATH = 'out.pickle'
+LOAD_PATH = ''
 SAVE = True
 LOAD = False
-SAVE_PATH = 'out.pickle'
+FINAL = True
+SAVE_PATH = ''
+EXT = '.pickle'
+functions = [
+    # 'DC Voltage',
+    'DC Current',
+    # 'AC Voltage',
+    # 'AC Current',
+    'Resistance 2W',
+    # 'Resistance 4W',
+    # 'Temperature',
+    # 'Frequency',
+    # 'Period',
+    'Continuity',
+    'Capacitance',
+    'Diode',
+    'DCV Ratio',
+    # 'Digitize Voltage',
+    # 'Digitize Current',
+]
 
 if __name__ == '__main__':
     dut = Session(IP)
-    interp = Crawler(Parser(dut))
-    if LOAD:
-        graph = pickle.load(open(SAVE_PATH, 'rb'))
-        interp.graph = graph
-        interp.analyze()
-    else:
-        graph = interp.crawl()
-    if graph.fully_explored():
-        print('Fully explored')
-    else:
-        print('INCOMPLETE')
-        print('Exploration was blocked by unexpected behavior')
-    for screen in graph:
-        print(str(screen) + ' has buttons:')
-        for button in screen.buttonmap.values():
-            print('    ' + button.name)
-        if len(set(graph.explored_edges(screen))) > 0:
-            print('  and points:')
-            for edge in graph.explored_edges(screen):
-                print('    through ' + str(edge) + ' to ' + str(graph.follow_edge(screen, edge)))
-    if SAVE and not LOAD:
-        pickle.dump(graph, open(SAVE_PATH, 'wb'), protocol=2)
-    data = {}  # dict from screen to (dict from button to helpstring)
-    for screen in graph:
-        if isinstance(screen, NumericalEntry):
-            break
-        print('going to ' + str(screen))
-        data[screen] = {}
-        interp.goto(screen)
-        print interp.currentscreen
-        print screen
-        assert interp.currentscreen == screen
-        buttons = screen.buttonmap.values()
-        for button in buttons:
-            print('        TRY: ' + str(screen) + ' :: ' + str(button))
-            # make focus on button
-            if interp.parser.get_focus() != button:
-                interp.click(Click(button))
-                interp.goto(screen)
-                assert interp.currentscreen == screen
-            if interp.parser.get_focus() == button:  # if false, then this button cannot be selected on entry
-                # get and store help text
-                interp.parser.press('help')
-                text = interp.parser.get_help_text()
-                data[screen][button] = text
-                interp.parser.press('exit')
-                print('        DONE: ' + str(screen) + ' :: ' + str(button) + ' :: ' + str(data[screen][button]))
-            else:
-                print('        FAIL: ' + str(screen) + ' :: ' + str(button))
+    parser_t = Parser(dut)
+    for function in functions:
+        interp = Crawler(parser_t)
+        if LOAD:
+            graph = pickle.load(open(SAVE_PATH + function + ' graph' + EXT, 'rb'))
+            interp.graph = graph
+        else:
+            interp.parser.press('menu')
+            interp.parser.select_function(function)
+            interp.parser.menu_click('Measure: Settings')
+            interp.crawl()
+            graph = interp.graph
+        if graph.fully_explored():
+            print('Fully explored ' + function)
+        else:
+            print(function + 'INCOMPLETE')
+            print('Exploration was blocked by unexpected behavior')
+        for screen in graph:
+            print(str(screen) + ' has buttons:')
+            for button in screen.buttonmap.values():
+                print('    ' + button.name)
+            if len(set(graph.explored_edges(screen))) > 0:
+                print('  and points:')
+                for edge in graph.explored_edges(screen):
+                    print('    through ' + str(edge) + ' to ' + str(graph.follow_edge(screen, edge)))
+        if SAVE and not LOAD:
+            pickle.dump(graph, open(SAVE_PATH + function + ' graph' + EXT, 'wb'), protocol=2)
+        data = {}  # dict from screen to (dict from button to helpstring)
+        for screen in graph:
+            if isinstance(screen, NumericalEntry):
+                break
+            print('going to ' + str(screen))
+            data[screen] = {}
+            interp.goto(screen)
+            print(interp.currentscreen)
+            print(screen)
+            assert interp.currentscreen == screen
+            buttons = screen.buttonmap.values()
+            for button in buttons:
+                print('        TRY: ' + str(screen) + ' :: ' + str(button))
+                # make focus on button
+                if interp.parser.get_focus() != button:
+                    interp.click(Click(button))
+                    interp.goto(screen)
+                    assert interp.currentscreen == screen
+                if interp.parser.get_focus() == button:  # if false, then this button cannot be selected on entry
+                    # get and store help text
+                    interp.parser.press('help')
+                    text = interp.parser.get_help_text()
+                    data[screen][button] = text
+                    interp.parser.press('exit')
+                    print('        DONE: ' + str(screen) + ' :: ' + str(button) + ' :: ' + str(data[screen][button]))
+                else:
+                    print('        FAIL: ' + str(screen) + ' :: ' + str(button))
 
-    if SAVE:
-        pickle.dump(data, open(SAVE_PATH + '(2)', 'wb'), protocol=2)
+        if SAVE:
+            pickle.dump(data, open(SAVE_PATH + function + ' help' + EXT, 'wb'), protocol=2)
 
-    for screen in data:
-        for button in data[screen]:
-            print(str(screen) + ' :: ' + str(button) + ' :: ' + str(data[screen][button]))
+        for screen in data:
+            for button in data[screen]:
+                print(str(screen) + ' :: ' + str(button) + ' :: ' + str(data[screen][button]))
+    if FINAL:
+        fout = open(SAVE_PATH + 'help' + '.csv', 'wb')
+        print('Parent Screen, Screen, Button, Help Caption', file=fout)
+        for function in functions:
+            data = pickle.load(open(SAVE_PATH + function + ' help' + EXT, 'rb'))
+            for screen in data:
+                for button in data[screen]:
+                    if screen.parent:
+                        print(str(screen.parent.title) + ', ' + str(screen.title) + ', ' + str(button.name) + ', ' + str(data[screen][button]), file=fout)
+                    else:
+                        print(', ' + str(screen.title) + ', ' + str(button.name) + ', ' + str(data[screen][button]), file=fout)
+        fout.close()
 
